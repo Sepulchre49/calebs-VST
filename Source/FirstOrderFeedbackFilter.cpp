293ed13cc6835c3676ca6184c481a41886bc51ab
CalebF98 <cfringer98@gmail.com>
2023-10-08 15:55:52 -0700

    Renamed FIR

---
+++ Source/FirstOrderFeedbackFilter.cpp
@@ -0,0 +1,28 @@
+/*
+  ==============================================================================
+
+    FirstOrderFeedforwardFilter.cpp
+    Created: 8 Oct 2023 2:15:25pm
+    Author:  Caleb
+
+  ==============================================================================
+*/
+
+#include "FIR.h"
+FirstOrderFeedforwardFilter::FirstOrderFeedforwardFilter() : a1{ 0.5 }, a2{ 0.5 } {
+
+};
+
+FirstOrderFeedforwardFilter::FirstOrderFeedforwardFilter(float coeffs) : a1{ coeffs }, a2{ coeffs } {
+
+};
+
+FirstOrderFeedforwardFilter::FirstOrderFeedforwardFilter(float a1, float a2) : a1{ a1 }, a2{ a2 } {
+
+};
+
+float FirstOrderFeedforwardFilter::process_sample(float z) {
+    float out = a1 * z + a2 * prevSample;
+    prevSample = z;
+    return out;
+}
