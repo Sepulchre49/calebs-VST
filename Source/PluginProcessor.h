36caa8fdaa7d405514cceec5227db8e237227620
CalebF98 <cfringer98@gmail.com>
2023-10-08 16:24:37 -0700

    Fixed include FIR header

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -9,7 +9,7 @@
 #pragma once
 
 #include <JuceHeader.h>
-#include "FIR.h"
+#include "FirstOrderFeedbackFilter.h"
 
 //==============================================================================
 /**
